fa4c6bdcf5712a20ec9b7e3f71e15f1afe62a896
jlhapp <jena.happ@duke.edu>
2016-07-27 11:40:09 -0400

    geocoding xy coordinates from table

--- csv.R
+++ csv.R
@@ -21,19 +21,43 @@ str(UNC)
 #Display the first three rows of the table
 head(UNC, 3)
 
+#map census tract shapefile of North Carolina
 Tracts <- readOGR(dsn = "D:/LearnR/BasicMapping/NC_Tracts.shp", layer = "NC_Tracts")
 plot(Tracts)
-gg
 
 
 
-#######Create a simple map
-points(UNC$X, UNC$Y)
+########Plot x,y coordinates using ggmap
+#Grab the center of a map and compute its bounding box.  
+#first geocode the center location
+gc<- geocode("Durham, North Carolina")
+#then map the geocoded point
+map <- get_map(gc)
+#Set the bounding box using the geocoded map
+(bb <- attr(map, "bb"))
+(bbox <- bb2bbox (bb))
 
-install.packages("ggplot")
+#Get the Stamenmap using the bounding box.
+stamMap <- get_stamenmap(bbox)
+
+#use the created map in ggmap and add the UNC table using the "geo_point"
+#expression.  You must have (data = [your table], aes(x=[the x column name],
+#y=[the y column name in your table]), color="[color]")
+ggmap(map) + geom_point(data = UNC, aes(x=X, y=Y), color="red")
+########Plot x,y coordinates using ggmap
+
+
+
+###Plot x,y coordinates using ggplot
 library(ggplot2)
+
+#Can map x,y coordinates by themselves in two ways.
 ggplot() + geom_point(data = UNC, aes(x=X, y=Y), color="red")
 
+#OR name a variable and use plot function
+map <- ggplot() + geom_point(data = UNC, aes(x=X, y=Y), color="red")
+plot(map)
+###Plot x,y coordinates using ggplot
 
 
 ###########################csv files#########################################
