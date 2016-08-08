ed43761c1a5de21b8afd3417936cb46cbede8363
jlhapp <jena.happ@duke.edu>
2016-08-08 14:50:09 -0400

    Cleaned up version of script

--- GeoJSON.R
+++ GeoJSON.R
@@ -4,15 +4,34 @@
 #GeoJSON is a JavaScript object with geographic data.  Also, leaflet is
 #designed to work natively with GeoJSON.
 
-
+library(shiny)
 library(rgdal)
 library(sp)
 library(ggmap)
-
+library(leaflet)
+library(jsonlite)
 
 ############################################################################
 ###########################GeoJSON files#####################################
+#Set the name "geojson" to a geojson file located on my D drive.
+Cat1 <- readLines("D:\\LearnR\\TryGEojson\\Cat1.geojson", warn = FALSE) %>%
+  paste(collapse = "\n") %>%
+  fromJSON(simplifyVector = FALSE)
 
+#Set the name "geojson" to a geojson file located on my D drive.  Cat2 does
+#not have as much data as Cat1
+Cat2 <- readLines("D:\\LearnR\\TryGEojson\\Cat2.geojson", warn = FALSE) %>%
+  paste(collapse = "\n") %>%
+  fromJSON(simplifyVector = FALSE)
+
+#To map the GeoJSON file to a specific extent, you'll first need to name a
+#variable.  Add leaft() %>% and then setView and specify the lat/long. Set 
+#the zoom level on a 1-17 scale. 1 = full zoom out, 17 = full zoom in
+viewmap <- leaflet() %>% setView(lng = -76.0589, lat = 34.9601, zoom = 7) 
+viewmap %>% addGeoJSON(Cat1) %>% addTiles()  
+
+#possibly feature.properties
+data <- getcom
 ###########################GeoJSON files#####################################
 ############################################################################
 
