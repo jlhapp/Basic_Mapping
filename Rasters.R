b8a1731714da658558aed1d5e399693684e4462b
jlhapp <jena.happ@duke.edu>
2016-08-08 13:55:29 -0400

    Raster Projections

--- Rasters.R
+++ Rasters.R
@@ -8,8 +8,6 @@ library(ggmap)
 
 ############################################################################
 ############################Rasters########################################
-Elev <- readGDAL("ned3m.img") # ERDAS Imagine .img file 
-
 install.packages("raster")
 library(raster)
 
@@ -20,20 +18,34 @@ climate <- raster("D:/DesktopStuff/R/Shapefiles/precipann_r_nc1.tif")
 #readGDAL returns a SpatialGridDataFrame and is defined by the sp package.
 #Visualize with spplot()
 climate2 <- readGDAL("D:/DesktopStuff/R/Shapefiles/precipann_r_nc1.tif")
+#readGDAL can read ERDAS Imagine file
+Elev <- readGDAL("ned3m.img") 
+
 
 #View the projection for the raster
 proj4string(climate2)
 
 #Map the raster with the appropriate expression.
-plot(climate)
+plot(climate3)
 spplot(climate2)
 
-###########################3Rasters########################################
+
+
+##TO SET A NEW PROJECTION FOR A RASTER
+#Import a raster
+climate3 <- raster("D:/DesktopStuff/R/Shapefiles/precipann_r_nc12.tif")
+
+#To view rojection information
+projection(climate3)
+
+#Create a variable and write the new projection inforamtion
+Proj2 <- "+proj=laea +lat_1=52 +lon_0=-10 +ellps=GRS80"
+
+#Transorm projection to another coordinate system using the projectRaster
+#function
+RasterProj <- projectRaster(climate3, crs = Proj2, method = 'bilinear')
+
+#Verify the projection is changed using the projection() function
+projection(RasterProj)
+###########################Rasters########################################
 ############################################################################
-
-
-
-
-
-#Creates a new table with Longitude and Latitude as column names
-coord <- cbind(UNC$Longitude, UNC$Latitude)
