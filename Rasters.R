13c55b2848781b0af625b589858fd6f10aa61e5b
jlhapp <jena.happ@duke.edu>
2016-08-25 12:09:07 -0400

    Update Rasters.R

--- Rasters.R
+++ Rasters.R
@@ -1,10 +1,13 @@
-###Basics of mapping using shapefiles, csv files with address information, 
-###and geojson file.
+#Basics of mapping rasters.  
+#-Use the Raster() function to import a single band raster.  Result will be "Formal class Raster Layer" 
+#-the stack() function for multiband rasters.  Result will be "Formal class RasterStacK"
+#-Use readGDAL() function.  Result will be "SpatialGridDataFrame"
 
 library(rgdal)
 library(sp)
 library(ggmap)
 
+if(!require(raster)) install.packages("raster")
 
 ############################################################################
 ############################Rasters########################################
@@ -13,11 +16,14 @@ library(raster)
 
 #Reference where the data is stored.  Raster returns a RasterLayer object
 #and is defined in the raster package.  Visualize with plot()
-climate <- raster("D:/DesktopStuff/R/Shapefiles/precipann_r_nc1.tif")
+#climate <- raster("D:/DesktopStuff/R/Shapefiles/precipann_r_nc1.tif")
+climate <- raster("data/precipann_r_nc1.tif")
 
 #readGDAL returns a SpatialGridDataFrame and is defined by the sp package.
 #Visualize with spplot()
-climate2 <- readGDAL("D:/DesktopStuff/R/Shapefiles/precipann_r_nc1.tif")
+#climate2 <- readGDAL("D:/DesktopStuff/R/Shapefiles/precipann_r_nc1.tif")
+climate2 <- readGDAL("data/precipann_r_nc1.tif")
+
 #readGDAL can read ERDAS Imagine file
 Elev <- readGDAL("ned3m.img") 
 
@@ -33,7 +39,8 @@ spplot(climate2)
 
 ##TO SET A NEW PROJECTION FOR A RASTER
 #Import a raster
-climate3 <- raster("D:/DesktopStuff/R/Shapefiles/precipann_r_nc12.tif")
+#climate3 <- raster("D:/DesktopStuff/R/Shapefiles/precipann_r_nc12.tif")
+climate3 <- raster("data/precipann_r_nc12.tif")
 
 #To view rojection information
 projection(climate3)
@@ -49,3 +56,29 @@ RasterProj <- projectRaster(climate3, crs = Proj2, method = 'bilinear')
 projection(RasterProj)
 ###########################Rasters########################################
 ############################################################################
+
+
+#Import precipitation raster for each month
+PrecipJan <- raster("data/precipjan_r_nc1.tif")
+PrecipFeb <- raster("data/precipfeb_r_nc1.tif")
+PrecipMar <- raster("data/precipmar_r_nc1.tif")
+PrecipApr <- raster("data/precipapr_r_nc1.tif")
+PrecipMay <- raster("data/precipmay_r_nc1.tif")
+PrecipJun <- raster("data/precipjun_r_nc1.tif")
+PrecipJuly <- raster("data/precipjul_r_nc1.tif")
+PrecipAug <- raster("data/precipaug_r_nc1.tif")
+PrecipSept <- raster("data/precipsep_r_nc1.tif")
+PrecipOct <- raster("data/precipoct_r_nc1.tif")
+PrecipNov <- raster("data/precipnov_r_nc1.tif")
+PrecipDec <- raster("data/precipdec_r_nc1.tif")
+
+
+#Use stack() function and combine all of the rasters.  Use object$ and you will 
+#get a list of all the raster.  You can then use plot() to view all rasters in stack
+rstack <- stack("data/precipnov_r_nc1.tif", "data/precipdec_r_nc1.tif")
+
+
+plot(rstack$precipdec_r_nc1)
+
+#Set animation to rasters using the raster stack object
+animate(rstack)
