ac7598dff9fd42c029813f82611f1ba3a3ca9d56
jlhapp <jena.happ@duke.edu>
2016-08-25 12:10:07 -0400

    Update Shapefile2GeoJSON.R

--- Shapefile2GeoJSON.R
+++ Shapefile2GeoJSON.R
@@ -5,16 +5,13 @@
 
 #For R script:
 #Use the shapefile of weather stations 
-library(tmap) # to read shapefile
 library(geojsonio) # to write geojson
 
 #Name variable shp and use read_shape 
-shp = read_shape("D:/LearnR/BasicMapping/WeatherStations.shp")
-#plot the shp data
-qtm(shp)
+shp = readOGR("data/WeatherStations.shp", "WeatherStations")
+
 #use the geojson_write expression followed by ([current file extension],
 #file = "[file path]")
 geojson_write(shp, file = "D:/tmp/shp.geojson")
 #######################Convert shapefile to GeoJSON############################
 #############################################################################
-
